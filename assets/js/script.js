08108641829aaf702e30c51b021cb9868a55093c
Joel Aguirre <Joels@Joshuas-MacBook-Pro.local>
2022-02-09 21:33:33 -0600

    need to fix it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,54 @@
-// TODO: Declare any global variables we need
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+let headCount = 0
+let tailsCount = 0
+// TODO: Refactor to use functions
 
 
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+document.addEventListener('DOMContentLoaded', function (e) {
 
+    document.querySelector('#flip').addEventListener('click',
+    function(e) {
+
+        if (Math.random() > 0.5){
+            console.log('heads')
+            //update image
+            let pennyFace = document.querySelector('#pennyFace')
+            pennyFace.src = 'assets/images/penny-heads.jpg'
+            pennyFace.alt = '2005 Penny, head side'
+            //Log outcome
+            document.querySelector('#message ').textContent = 'You Flipped Heads!'
+
+            //Update JS global vars
+            headCount++
+
+
+
+            document.querySelector('#heads').textContent = headCount
+            // #/total
+            document.querySelector('#heads-percent').textContent = Math.round(headCount/(headCount+tailsCount)*100) + '%' 
+            document.querySelector('#tails-percent').textContent = Math.round(tailsCount/(headCount+tailsCount)*100) + '%'
+
+        }else{
+            console.log('tails')
+        }
+    
+        console.log(Math.random())
+
+
+    })
+
+    document.querySelector('#clear').addEventListener('click', function(e) {
+     headCount = 0
+     tailsCount = 0
+     document.querySelector('#message') = "Let's get Rolling!"
+     document.querySelector('#heads').textContent = 0
+     document.querySelector('#tails').textContent = 0
+     document.querySelector('#heads-percent').textContent = '0%'
+     document.querySelector('#tails-percent').textContent = '0%'
+     document.querySelector('#pennyFace').src = 'assets/images/penny-heads.jpg'
+     document.querySelector('#pennyFace').alt = '2005 penny, heads side'
+    })
 })
+
+
